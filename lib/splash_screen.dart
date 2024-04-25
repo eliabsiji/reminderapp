cd6bf44bc4045eb6ddc1eca7c6da5f0c150ef276
eliab <eliabsiji@gmail.com>
2024-04-25 22:53:00 +0100

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,7 +2,6 @@
 
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:reminderapp/screens/login.dart';
 import 'package:reminderapp/slider.dart';
 
 class SplashScreen extends StatefulWidget {
